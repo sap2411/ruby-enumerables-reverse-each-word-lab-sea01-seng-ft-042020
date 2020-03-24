f85b7157c04ff9c1dba6a0203ca0a8d312b3adc5
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-24 12:19:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word_with_each(string)
   original_array = string.split(" ")
   return_array = []
-  original_array.count {|string| return_array << string.reverse}
+  original_array.collect {|string| return_array << string.reverse}
   return_array.join(" ")
 end
 
