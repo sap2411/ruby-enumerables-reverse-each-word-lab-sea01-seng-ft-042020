10d7cb64bd4f9994dcd7e0306174c00f5cb363cb
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-24 12:20:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,3 @@
-def reverse_each_word_with_each(string)
-  original_array = string.split(" ")
-  return_array = []
-  original_array.each {|string| return_array << string.reverse}
-  return_array.join(" ")
-end
 
 def reverse_each_word(string)
   array = string.split(" ")
